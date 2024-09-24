d080dfbfe863c78b5311da911f7cb2becf658085
VinhSuki <vinhbestyasuo@gmail.com>
2024-09-25 00:33:53 +0700

    fix some problems

--- assets/js/avt.js
+++
@@ -1,5 +0,0 @@
-const avt = document.querySelector('#avt')
-const propper = document.querySelector('.header__propper')
-avt.addEventListener('click',(e)=>{
-     propper.classList.toggle('active');
-})
